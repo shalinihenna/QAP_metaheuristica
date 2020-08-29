2867509244da7f319c00719f91ae83be92ad67b0
shali7296 <shalini.ramchandani@usach.cl>
2020-08-29 18:11:29 -0400

    Objective Function added

--- AG.py
+++ AG.py
@@ -1,9 +1,12 @@
 import random
 
+#VARIABLES GLOBALES
 population = [] #Arreglo de arreglos
 results = [] #Arreglo de mejor función objetivo de cada generacion
+flow = [] #Datos de la matriz de flujo
+distance = [] #Datos de la matriz de distancias
 
-
+#FUNCIONES
 def readFile(name):
     name = "data/"+name + ".txt"
     file = open(name,'r')
@@ -19,7 +22,7 @@ def readFile(name):
     print (matrix)
     return matrix
 
-#Falta revisar repetidos 
+#Falta revisar repetidos
 def initialPopulation(maxIndividuals, positions):
 	i = 0
 	while i < maxIndividuals:
@@ -30,7 +33,38 @@ def initialPopulation(maxIndividuals, positions):
 		population.append(individual)
 		i += 1
 
+def objectiveFunction(solution):
+    sum = 0
+    for positionI, elementI in enumerate(solution): #position = ubicacion // elemento = instalacion
+        for positionJ, elementJ in enumerate(solution):
+            aux = flow[elementI-1][elementJ-1]*distance[positionI][positionJ]
+            sum += aux
+    return sum
+
+
+#PRUEBAS------------------------
+#flow = [[0,3,0,2],[3,0,0,1],[0,0,0,4],[2,1,4,0]]
+#distance = [[0,22,53,0],[22,0,40,0],[53,40,0,55],[0,0,55,0]]
+#hola = objectiveFunction([2,1,4,3])
+#print (hola)
+#PRUEBAS------------------------
+
+#EJECUCIÓN MAIN (PRINCIPAL)
+#readFile("Dchr12a.dat")
+#readFile("Dchr12a.dat")
+#readFile("Dchr12a.dat")
+#readFile("Dchr12a.dat")
 
 #initialPopulation(10,12)
 #print (population)
-#readFile("Dchr12a.dat")
+#1. readFile
+#2. initialPopulation
+#3. Pedir generation máxima maxGeneration
+#generation = 0
+#while generation < maxGeneration
+#a. Evaluar
+#b. Seleccion
+#c. Reproduccion
+#    - Recombinacion
+#    - Mutacion
+#d. Reemplazo
