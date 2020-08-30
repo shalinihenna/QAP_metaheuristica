a393b04e7fd7e79e3cd09256473bda4d2a46a075
shali7296 <shalini.ramchandani@usach.cl>
2020-08-29 20:29:36 -0400

    ObjectiveFunction fixed

--- AG.py
+++ AG.py
@@ -19,7 +19,6 @@ def readFile(name):
             rowInt.append(int(element))
         matrix.append(rowInt)
     file.close()
-    print (matrix)
     return matrix
 
 #Falta revisar repetidos
@@ -33,20 +32,43 @@ def initialPopulation(maxIndividuals, positions):
 		population.append(individual)
 		i += 1
 
+'''
 def objectiveFunction(solution):
     sum = 0
+    print(list(enumerate(solution)))
     for positionI, elementI in enumerate(solution): #position = ubicacion // elemento = instalacion
+        print ("i: ", positionI, elementI-1)
         for positionJ, elementJ in enumerate(solution):
+            print ("j: ", positionJ, elementJ-1)
             aux = flow[elementI-1][elementJ-1]*distance[positionI][positionJ]
+            #print ("aux: ",aux)
+            sum += aux
+        print ("suma de iteracion i",positionI,": ",sum)
+        print()
+    return sum
+'''
+
+def objectiveFunction(solution):
+    sum = 0
+    n = len(solution)
+    for i in range(n):
+        for j in range(n):
+            aux = flow[i][j] * distance[solution[i]-1][solution[j]-1]
             sum += aux
     return sum
-
 
 #PRUEBAS------------------------
 #flow = [[0,3,0,2],[3,0,0,1],[0,0,0,4],[2,1,4,0]]
 #distance = [[0,22,53,0],[22,0,40,0],[53,40,0,55],[0,0,55,0]]
-#hola = objectiveFunction([2,1,4,3])
+flow = readFile("Fchr18a.dat")
+distance = readFile("Dchr18a.dat")
+#hola = objectiveFunction([7,5,12,2,1,3,9,11,10,6,8,4])
+#hola2 = funcionObjetivo([7,5,12,2,1,3,9,11,10,6,8,4],flow,distance)
+#hola = objectiveFunction([3,13,6,4,18,12,10,5,1,11,8,7,17,14,9,16,15,2])
+#hola2 = funcionObjetivo([3,13,6,4,18,12,10,5,1,11,8,7,17,14,9,16,15,2],flow,distance)
 #print (hola)
+#print(hola2)
+#print(hola2)
 #PRUEBAS------------------------
 
 #EJECUCIÓN MAIN (PRINCIPAL)
